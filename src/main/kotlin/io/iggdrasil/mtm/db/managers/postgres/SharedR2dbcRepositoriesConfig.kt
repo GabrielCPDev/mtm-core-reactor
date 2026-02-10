c1be4c4d2f5effda435349ef84d9f5205e9e7ddb
GabrielCPDev <gabrielcpcontato@outlook.com>
2026-02-10 09:28:37 -0300

    Adicionando configuracao para gerenciamento condicional de beans baseado no banco escolhido

--- src/main/kotlin/io/iggdrasil/mtm/db/managers/postgres/SharedR2dbcRepositoriesConfig.kt
+++ src/main/kotlin/io/iggdrasil/mtm/db/managers/postgres/SharedR2dbcRepositoriesConfig.kt
@@ -1,12 +1,14 @@
 package io.iggdrasil.mtm.db.managers.postgres
 
 import io.r2dbc.spi.ConnectionFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @Configuration
+@ConditionalOnProperty(prefix = "mtm.data-source", name = ["type"], havingValue = "POSTGRES")
 @EnableR2dbcRepositories(
     basePackages = ["\${mtm.repositories.shared-packages}"],
     entityOperationsRef = "sharedEntityTemplate"
